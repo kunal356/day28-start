f31d8aa64ffb85b819042f29c644a466c2b4419a
Kunal Gaikwad <gaikwadkunal610@gmail.com>
2024-03-22 20:35:04 +0000

    258. Challenge - Complete the Application's User Interface (UI)

--- main.py
+++ main.py
@@ -20,9 +20,22 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.config(padx=100, pady=50, bg=YELLOW)
+
+timer_label = Label(text="Timer", font=(FONT_NAME, 50, 'bold'), fg=GREEN, bg=YELLOW)
+timer_label.grid(column=1, row=0)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, 'bold'))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+checkmark_label = Label(text="✅", bg=YELLOW, fg=GREEN)
+checkmark_label.grid(column=1, row=3)
 window.mainloop()
