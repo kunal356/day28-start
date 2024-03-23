a14b86103ff8f328489d3f2d4145740184392242
Kunal Gaikwad <gaikwadkunal610@gmail.com>
2024-03-23 10:14:03 +0000

    259. Add a Count Down Mechanism

--- main.py
+++ main.py
@@ -10,11 +10,23 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def start_timer():
+    count_down(WORK_MIN * 60)
+
+
+def count_down(count):
+    count_min = round(count / 60)
+    count_sec = round(count % 60)
+    if count > 0:
+        canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -27,10 +39,12 @@ timer_label.grid(column=1, row=0)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, 'bold'))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, 'bold'))
 canvas.grid(column=1, row=1)
 
-start_button = Button(text="Start", highlightthickness=0)
+# count_down(5)
+
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset", highlightthickness=0)
